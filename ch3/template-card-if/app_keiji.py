17fd6e8eaeaebdd0a8540e9334819f6fe066afe9
yochimonji <yochimonji@gmail.com>
2021-02-25 14:47:39 +0900

    initial commit

---
+++ ch3/template-card-if/app_keiji.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    username = 'ケイジ'
+    age = 19
+    email = 'keiji@example.com'
+    return render_template('card-age.html',
+        username=username,
+        age=age,
+        email=email)
+
+if __name__ == '__main__':
+    app.run(debug=True)
